88082a3c6d1f74938fe813f6e801765c81575a35
worrysjh <97208639+worrysjh@users.noreply.github.com>
2025-04-17 00:52:16 +0900

    Update events.js
    주석 추가

--- src/action/events.js
+++ src/action/events.js
@@ -1,10 +1,12 @@
 import { getTodos, saveTodos } from "./storage.js";
 import { renderTodos } from "./ui.js";
 
+// 새로운 할 일 추가를 위한 팝업창을 띄우는 함수
 export function openTodoPopup() {
   window.open("src/popup/todolist.html", "일정 추가", "width=400,height=550");
 }
 
+// 기존 할 일 수정을 위한 팝업창을 띄우는 함수
 export function editTodo(index) {
   window.open(
     `src/popup/todolist.html?edit=${index}`,
@@ -13,6 +15,7 @@ export function editTodo(index) {
   );
 }
 
+// 지정한 인덱스의 할 일을 삭제하는 함수
 export function deleteTodo(index) {
   const todos = getTodos();
   todos.splice(index, 1);
